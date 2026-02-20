fa3e58c88402551e882a615944db585e1ee934b5
Harel <harel.olguin@iteso.mx>
2026-02-19 18:51:34 -0600

    feat: initial rendering

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let counter = $state(0);
+</script>
+
+<div class="p-5">
+	<button
+		class="cursor-pointer rounded-2xl bg-red-500 px-1 py-2 text-white transition-colors hover:bg-red-400"
+		onclick={() => counter++}>Counter: {counter}</button
+	>
+</div>
